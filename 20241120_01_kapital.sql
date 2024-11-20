63b35cbd34a134c8a5444516fbf0f770380f1db6
AndreyBazhin <85554995+AndreyBazhin@users.noreply.github.com>
2024-11-20 17:34:47 +0300

    20.11.2024

---
+++ 20241120_01_kapital.sql
@@ -0,0 +1,39 @@
+﻿CREATE VIEW schet.vw_act
+AS
+SELECT
+  *
+FROM vrschet.EXPERT.ActOut
+GO
+
+CREATE FUNCTION schet.SMOPrefix ()
+RETURNS VARCHAR(5)
+BEGIN
+  DECLARE @res VARCHAR(5)
+  SELECT
+    @res = value
+  FROM vrschet.ART.options
+  WHERE paramname = 'SMOPrefix'
+  RETURN @res
+END 
+
+CREATE VIEW schet.vw_videksp 
+AS SELECT ve.*
+FROM vrschet.ART.VidEksp ve
+
+CREATE VIEW dict.vw_users
+AS 
+SELECT t.KodUser
+      ,t.FIO COLLATE Cyrillic_General_100_CI_AS AS fio
+      ,t.KodFil
+      ,t.Uvolen
+      ,t.SMO
+FROM smo_treg.dbo.TRUsers t
+WHERE t.KodFil = config.KodSMO() AND t.KodUser > 1000
+UNION
+SELECT t.KodUser
+      ,t.FIO COLLATE Cyrillic_General_100_CI_AS
+      ,t.KodFil
+      ,t.Uvolen
+      ,t.SMO
+FROM vrschet.DICT.TRUsers t
+WHERE t.KodFil = config.KodSMO() AND t.KodUser > 1000
---
+++ 20241120_02.sql
@@ -0,0 +1,41 @@
+﻿USE smo_work
+GO
+
+IF DB_NAME() <> N'smo_work' SET NOEXEC ON
+GO
+
+--
+-- Начать транзакцию
+--
+BEGIN TRANSACTION
+GO
+
+--
+-- Удалить столбец [FIO] из таблицы [config].[users]
+--
+ALTER TABLE config.users
+  DROP COLUMN IF EXISTS FIO
+GO
+IF @@ERROR<>0 OR @@TRANCOUNT=0 BEGIN IF @@TRANCOUNT>0 ROLLBACK SET NOEXEC ON END
+GO
+
+--
+-- Удалить столбец [dismissed] из таблицы [config].[users]
+--
+ALTER TABLE config.users
+  DROP COLUMN IF EXISTS dismissed
+GO
+IF @@ERROR<>0 OR @@TRANCOUNT=0 BEGIN IF @@TRANCOUNT>0 ROLLBACK SET NOEXEC ON END
+GO
+
+--
+-- Фиксировать транзакцию
+--
+IF @@TRANCOUNT>0 COMMIT TRANSACTION
+GO
+
+--
+-- Установить NOEXEC в состояние off
+--
+SET NOEXEC OFF
+GO
